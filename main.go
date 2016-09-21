4074662af62a43230dbcdfe05fd260790bd2bb59
Minh-Quan TRAN <mq.tran@prismamedia.com>
2016-09-21 18:27:28 +0200

    update

--- main.go
+++ main.go
@@ -23,8 +23,36 @@ func main() {
 
 		for _, programme := range result.Programme {
 			channel := findChannel(programme.Attr_channel, result.Channel)
-			str += fmt.Sprintf("%+v-%+v %+v on %+v\n", programme.Attr_start, programme.Attr_stop, programme.Title.Text, channel.Display_name[len(channel.Display_name)-1].Text)
+			str += fmt.Sprintf(
+				"%+v-%+v %s on %s\n",
+				programme.Attr_start,
+				programme.Attr_stop,
+				programme.Title.Text,
+				channel.Display_name[len(channel.Display_name)-1].Text,
+			)
+			if programme.Credits != nil {
+				if len(programme.Credits.Director) > 0 {
+					str += fmt.Sprint("Director\n")
+					for _, director := range programme.Credits.Director {
+						str += fmt.Sprintf("- %s\n", director.Text)
+					}
+				}
 
+				if len(programme.Credits.Actor) > 0 {
+					str += fmt.Sprint("Actors\n")
+					for _, actor := range programme.Credits.Actor {
+						str += fmt.Sprintf("- %s\n", actor.Text)
+					}
+				}
+
+				if len(programme.Credits.Presenter) > 0 {
+					str += fmt.Sprint("Presenters\n")
+					for _, presenter := range programme.Credits.Presenter {
+						str += fmt.Sprintf("- %s\n", presenter.Text)
+					}
+				}
+			}
+			str += fmt.Sprintf("\n")
 		}
 		ctx.Text(iris.StatusOK, str)
 	})
