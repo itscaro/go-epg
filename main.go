72e8d3034dd8895d042f0f8e8f7e4764c419e79d
Minh-Quan Tran <itscaro@users.noreply.github.com>
2016-12-19 17:49:41 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"html/template"
@@ -8,6 +9,9 @@ import (
 	"strconv"
 	"time"
 
+	"golang.org/x/net/html/charset"
+
+	"github.com/iris-contrib/middleware/logger"
 	"github.com/kataras/go-template/html"
 	"github.com/kataras/iris"
 )
@@ -18,11 +22,13 @@ const (
 )
 
 type MyProgramme struct {
-	Title    string
-	Channel  string
-	Start    time.Time
-	End      time.Time
-	DivStyle string
+	Title        string
+	Channel      string
+	Start        time.Time
+	End          time.Time
+	DivStyle     string
+	RawProgramme *Programme
+	RawChannel   *Channel
 }
 
 func main() {
@@ -45,71 +51,27 @@ func main() {
 		},
 	}))
 
+	iris.Use(logger.New())
+
+	iris.Get("/prime", func(ctx *iris.Context) {
+		_, epgByDate := processData()
+
+		for _, pByChannel := range epgByDate[time.Now().Format("20060102")] {
+			for _, p := range pByChannel {
+				length := 0
+				if p.RawProgramme.Length != nil {
+					length, _ = strconv.Atoi(p.RawProgramme.Length.Text)
+				}
+				if p.Start.Hour() > 18 && length > 45 {
+					ctx.Write("%s %s %s: %s\n", p.Title, p.Channel, p.Start.Format(timeLayout), p.RawProgramme.Desc.Text)
+				}
+			}
+		}
+	})
+
 	//iris.Favicon("./favicon.ico")
 	iris.Get("/", func(ctx *iris.Context) {
-		var result Tv
-		data, _ := ioutil.ReadFile("./xmltvutf8.xml")
-		xml.Unmarshal(data, &result)
-
-		epgByChannel := make(map[string]map[string][]MyProgramme)
-		epgByDate := make(map[string]map[string][]MyProgramme)
-		var str string
-
-		for _, programme := range result.Programme {
-			channel := findChannel(programme.Attr_channel, result.Channel)
-			channelName := channel.Display_name[len(channel.Display_name)-1].Text
-			start, _ := time.Parse(timeXmlLayout, programme.Attr_start)
-			end, _ := time.Parse(timeXmlLayout, programme.Attr_stop)
-			str = fmt.Sprintf(
-				"%+v-%+v %s on %s\n",
-				start.Format(timeLayout),
-				end.Format(timeLayout),
-				programme.Title.Text,
-				channel.Display_name[len(channel.Display_name)-1].Text,
-			)
-			if programme.Credits != nil {
-				if len(programme.Credits.Director) > 0 {
-					str += fmt.Sprint("Director\n")
-					for _, director := range programme.Credits.Director {
-						str += fmt.Sprintf("- %s\n", director.Text)
-					}
-				}
-
-				if len(programme.Credits.Actor) > 0 {
-					str += fmt.Sprint("Actors\n")
-					for _, actor := range programme.Credits.Actor {
-						str += fmt.Sprintf("- %s\n", actor.Text)
-					}
-				}
-
-				if len(programme.Credits.Presenter) > 0 {
-					str += fmt.Sprint("Presenters\n")
-					for _, presenter := range programme.Credits.Presenter {
-						str += fmt.Sprintf("- %s\n", presenter.Text)
-					}
-				}
-			}
-			str += fmt.Sprintf("\n")
-
-			styleWidth := end.Sub(start).Minutes() / 60 / 24 * 100
-			p := MyProgramme{
-				Title:    programme.Title.Text,
-				Channel:  channel.Display_name[len(channel.Display_name)-1].Text,
-				Start:    start,
-				End:      end,
-				DivStyle: "width: " + strconv.FormatFloat(styleWidth, 'f', 2, 64) + "%;",
-			}
-			//fmt.Printf("%+v", p)
-			if epgByChannel[channelName] == nil {
-				epgByChannel[channelName] = make(map[string][]MyProgramme)
-			}
-			if epgByDate[start.Format("20060102")] == nil {
-				epgByDate[start.Format("20060102")] = make(map[string][]MyProgramme)
-			}
-
-			epgByChannel[channelName][start.Format("20060102")] = append(epgByChannel[channelName][start.Format("20060102")], p)
-			epgByDate[start.Format("20060102")][channelName] = append(epgByDate[start.Format("20060102")][channelName], p)
-		}
+		epgByChannel, epgByDate := processData()
 
 		by := ctx.URLParam("by")
 		switch by {
@@ -133,4 +95,78 @@ func findChannel(id string, channels []*Channel) *Channel {
 	}
 
 	return nil
+}
+
+func processData() (epgByChannel map[string]map[string][]MyProgramme, epgByDate map[string]map[string][]MyProgramme) {
+	var result Tv
+
+	data, _ := ioutil.ReadFile("./tnt.xml")
+	decoder := xml.NewDecoder(bytes.NewReader(data))
+	decoder.CharsetReader = charset.NewReaderLabel
+	if err := decoder.Decode(&result); err != nil {
+		iris.Logger.Panicln(err)
+	}
+
+	epgByChannel = make(map[string]map[string][]MyProgramme)
+	epgByDate = make(map[string]map[string][]MyProgramme)
+
+	for _, programme := range result.Programme {
+		channel := findChannel(programme.Attr_channel, result.Channel)
+		channelName := channel.Display_name[len(channel.Display_name)-1].Text
+		start, _ := time.Parse(timeXmlLayout, programme.Attr_start)
+		end, _ := time.Parse(timeXmlLayout, programme.Attr_stop)
+		str := fmt.Sprintf(
+			"%+v-%+v %s on %s\n",
+			start.Format(timeLayout),
+			end.Format(timeLayout),
+			programme.Title.Text,
+			channel.Display_name[len(channel.Display_name)-1].Text,
+		)
+		if programme.Credits != nil {
+			if len(programme.Credits.Director) > 0 {
+				str += fmt.Sprint("Director\n")
+				for _, director := range programme.Credits.Director {
+					str += fmt.Sprintf("- %s\n", director.Text)
+				}
+			}
+
+			if len(programme.Credits.Actor) > 0 {
+				str += fmt.Sprint("Actors\n")
+				for _, actor := range programme.Credits.Actor {
+					str += fmt.Sprintf("- %s\n", actor.Text)
+				}
+			}
+
+			if len(programme.Credits.Presenter) > 0 {
+				str += fmt.Sprint("Presenters\n")
+				for _, presenter := range programme.Credits.Presenter {
+					str += fmt.Sprintf("- %s\n", presenter.Text)
+				}
+			}
+		}
+		str += fmt.Sprintf("\n")
+
+		styleWidth := end.Sub(start).Minutes() / 60 / 24 * 100
+		p := MyProgramme{
+			Title:        programme.Title.Text,
+			Channel:      channel.Display_name[len(channel.Display_name)-1].Text,
+			Start:        start,
+			End:          end,
+			DivStyle:     "width: " + strconv.FormatFloat(styleWidth, 'f', 2, 64) + "%;",
+			RawProgramme: programme,
+			RawChannel:   channel,
+		}
+		//fmt.Printf("%+v", p)
+		if epgByChannel[channelName] == nil {
+			epgByChannel[channelName] = make(map[string][]MyProgramme)
+		}
+		if epgByDate[start.Format("20060102")] == nil {
+			epgByDate[start.Format("20060102")] = make(map[string][]MyProgramme)
+		}
+
+		epgByChannel[channelName][start.Format("20060102")] = append(epgByChannel[channelName][start.Format("20060102")], p)
+		epgByDate[start.Format("20060102")][channelName] = append(epgByDate[start.Format("20060102")][channelName], p)
+	}
+
+	return
 }
